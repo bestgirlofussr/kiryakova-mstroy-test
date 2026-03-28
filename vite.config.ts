97b3a9835152e2fd02d0fb4c83cbc03fa49c2beb
YKiryakova <julie.kiryakova@gmail.com>
2026-03-28 23:46:43 +0300

    update chunkSizeWarningLimit

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   server: {
     port: 8080,
     open: true,
